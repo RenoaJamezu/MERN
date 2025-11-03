bc5ae3d2b0cc84286529ab218b620ba7b0931649
Kurasao <jamerolenor@gmail.com>
2025-11-03 21:06:44 +0800

    Add delete functionality to WorkoutDetails component and update workoutsReducer to handle DELETE_WORKOUT action

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,27 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      console.log('workout deleted', json)
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
       <p><strong>Sets: </strong>{workout.sets}</p>
       <p><strong>Reps: </strong>{workout.reps}</p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   )
 }
